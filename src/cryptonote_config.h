aa888f7d8fc43971e185b3d8ae6769ce48082213
pranavms13 <52594065+pranavms13@users.noreply.github.com>
2019-07-07 03:31:18 +0530

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -193,7 +193,7 @@ namespace config
     boost::uuids::uuid const NETWORK_ID = { {
         0x11 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x21, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
       } }; // Bender's daydream
-    std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+    std::string const GENESIS_TX = "013c01ff0001ffffffffffff030298c8a6ecd0b9ff6913bbc3fade6eb9e926ec19e83fb4dace6fab44d719c618f02101c3a09b20a14a5685105e7132beaa90ebd40349f04ac6b10d095c6ba570ae9d94";
     uint32_t const GENESIS_NONCE = 10001;
   }
 
