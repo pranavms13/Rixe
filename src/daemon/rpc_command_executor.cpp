4a9c1323d1d8f30592bf0bb59bea02527510c6c3
pranavms13 <52594065+pranavms13@users.noreply.github.com>
2019-07-06 20:33:39 +0530

    Update rpc_command_executor.cpp

--- src/daemon/rpc_command_executor.cpp
+++ src/daemon/rpc_command_executor.cpp
@@ -558,8 +558,8 @@ bool t_rpc_command_executor::mining_status() {
     uint64_t daily = 86400ull / mres.block_target * mres.block_reward * ratio;
     uint64_t monthly = 86400ull / mres.block_target * 30.5 * mres.block_reward * ratio;
     uint64_t yearly = 86400ull / mres.block_target * 356 * mres.block_reward * ratio;
-    tools::msg_writer() << "Expected: " << cryptonote::print_money(daily) << " monero daily, "
-        << cryptonote::print_money(monthly) << " monero monthly, " << cryptonote::print_money(yearly) << " yearly";
+    tools::msg_writer() << "Expected: " << cryptonote::print_money(daily) << " rixe daily, "
+        << cryptonote::print_money(monthly) << " rixe monthly, " << cryptonote::print_money(yearly) << " yearly";
   }
 
   return true;
@@ -1299,8 +1299,8 @@ bool t_rpc_command_executor::stop_daemon()
 //# ifdef WIN32
 //    // Stop via service API
 //    // TODO - this is only temporary!  Get rid of hard-coded constants!
-//    bool ok = windows::stop_service("BitMonero Daemon");
-//    ok = windows::uninstall_service("BitMonero Daemon");
+//    bool ok = windows::stop_service("Rixe Daemon");
+//    ok = windows::uninstall_service("Rixe Daemon");
 //    //bool ok = windows::stop_service(SERVICE_NAME);
 //    //ok = windows::uninstall_service(SERVICE_NAME);
 //    if (ok)
@@ -1344,10 +1344,10 @@ bool t_rpc_command_executor::print_status()
   bool daemon_is_alive = m_rpc_client->check_connection();
 
   if(daemon_is_alive) {
-    tools::success_msg_writer() << "monerod is running";
+    tools::success_msg_writer() << "rixed is running";
   }
   else {
-    tools::fail_msg_writer() << "monerod is NOT running";
+    tools::fail_msg_writer() << "rixed is NOT running";
   }
 
   return true;
