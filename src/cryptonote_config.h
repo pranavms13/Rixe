cfa55b5b0d5f469e0a2f328e33c8be53133c56a5
Pranav M S <52594065+pranavms13@users.noreply.github.com>
2019-07-09 17:37:44 +0530

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -179,7 +179,7 @@ namespace config
   boost::uuids::uuid const NETWORK_ID = { {
       0x11 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x21, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
     } }; // Bender's nightmare
-  std::string const GENESIS_TX = "010a01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
+  std::string const GENESIS_TX = "010a01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210168208bae3ff73d94776e15fd94d7b4b7c5050aea5498ab99c8da63cda8b7a557";
   uint32_t const GENESIS_NONCE = 10000;
 
   namespace testnet
